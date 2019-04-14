3e13dafc7df3d66a03eb095c5fb39d3003ea2fe4
Andy Burne <rougesheep@gmail.com>
2019-04-14 14:14:45 +0100

    wrapper script to start/stop

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
-. ./venv/bin/activate
-nohup python3.7 -u wishingwall.py > python.log 2>&1 &
-echo $! > python.pid
+
+case "$1" in:
+    "start":
+        start()
+        ;;
+    "stop":
+        stop()
+        ;;
+    "restart":
+        stop()
+        start()
+        ;;
+    default:
+        echo "Usage: ./wrapper.sh <start|stop|restart>"
+        ;;
+esac
+
+function start {
+    . ./venv/bin/activate
+    nohup python3.7 -u wishingwall.py > python.log 2>&1 &
+    echo $! > python.pid
+}
+
+function stop {
+    kill $(cat python.pid) && rm -f python.pid
+}
