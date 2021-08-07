1245cbe7409a6a0966e270a6fb6d96970356a6b5
rougesheep <rougesheep@gmail.com>
2021-08-07 14:51:48 +0100

    remove old commands

--- jhongbot.py
+++ jhongbot.py
@@ -56,12 +56,6 @@ emoji = {
 client = MongoClient(config['database'])
 db = client['jhongbot']
 
-def isToBeAbused(username):
-    return username in config['abuseList']
-
-def getAbuse(username):
-    return random.choice(abuse).format(username)
-
 bot = commands.Bot(command_prefix='?', description='A pretty useless bot')
 
 @bot.event
@@ -80,10 +74,7 @@ async def history(message):
 
 @bot.command(hidden=True)
 async def hello(ctx):
-    if (isToBeAbused(str(ctx.author))):
-        message = getAbuse(ctx.author.mention)
-    else:
-        message = 'Hello {}!'.format(ctx.author.mention)
+    message = 'Hello {}!'.format(ctx.author.mention)
     await ctx.send(message)
 
 @bot.command(brief='Link to the GitHub page for this bot.')
@@ -113,16 +104,6 @@ async def wish(ctx, *msg: str):
     else:
         await ctx.message.add_reaction(random.choice(bad_reactions))
 
-@bot.command(brief='Guide for the Niobe Labs puzzle from Black Armoury.')
-async def niobe(ctx):
-    title = 'Niobe Labs puzzle'
-    img = 'https://i.imgur.com/qaPwWnZ.png'
-    embed = discord.Embed(title=title)
-    embed.set_image(url=img)
-    embed.add_field(name='Guide', value='https://gamerant.com/destiny-2-niobe-labs-puzzle-how-to-beat/', inline=False)
-    embed.add_field(name='Infographic', value='https://imgur.com/a/qQjV9zI', inline=False)
-    await ctx.send(embed=embed)
-
 @bot.command(brief='Dawning oven recipes.')
 async def dawning(ctx):
     title = 'Dawning Recipes'
@@ -130,36 +111,6 @@ async def dawning(ctx):
     embed = discord.Embed(title=title)
     embed.set_image(url=img)
     await ctx.send(embed=embed)
-
-@bot.command(brief='Menagerie chalice combinations.', aliases=['menagerie'])
-async def chalice(ctx, msg=''):
-    title = 'Chalice Combinations'
-    armour_img = 'https://i.imgur.com/6tdW1Fs.png'
-    weapon_img = 'https://i.imgur.com/EzOgjqy.png'
-    armour_embed = discord.Embed(title=title)
-    armour_embed.set_image(url=armour_img)
-    weapon_embed = discord.Embed(title=title)
-    weapon_embed.set_image(url=weapon_img)
-    if msg == 'armour':
-        await ctx.send(embed=armour_embed)
-    elif msg == 'weapons':
-        await ctx.send(embed=weapon_embed)
-    else:
-        await ctx.send('{} weapons or armour?'.format(ctx.author.mention))
-
-@bot.command(brief='Days before season end.')
-async def season(ctx):
-    season_name = 'the Worthy'
-    season_end = datetime(2020, 3, 10, 17, 00)
-    now = datetime.now()
-
-    s = (season_end - now).total_seconds()
-    
-    days, r1 = divmod(s, 86400)
-    hours, r2 = divmod(r1, 3600)
-
-    msg = "Season of {} ends in {:00} days {} hours".format(season_name, int(days), int(hours))
-    await ctx.send(msg)
 
 @bot.command(hidden=True, aliases=['meow', 'nyan', 'cat', '🐈', '🐱'])
 async def poncho(ctx):
@@ -175,48 +126,6 @@ async def ding(ctx):
     img = 'https://media.giphy.com/media/32681KwrcXqrFIpI0P/giphy.gif'
     embed = discord.Embed(title=title)
     embed.set_image(url=img)
-    await ctx.send(embed=embed)
-
-@bot.command(brief="Vendor Power Drops", aliases=['engrams'])
-async def vendors(ctx):
-    uri = "https://api.vendorengrams.xyz/getVendorDrops"
-    r = requests.get(url=uri)
-    vendors = r.json()
-    vendor_names = {
-        'devrim': 'Devrim Kay',
-        'bray': 'Ana Bray',
-        'asher': 'Asher Mir',
-        'benedict': 'Benedict 99-40',
-        'failsafe': 'Failsafe',
-        'zavala': 'Zavala',
-        'shaxx': 'Shaxx',
-        'banshee': 'Banshee-44',
-        'werner': 'Werner 99-40',
-        'sloane': 'Sloane',
-        'fanboy': 'Brother Vance',
-        'saladin': 'Lord Saladin'
-    }
-    good = []
-    bad = []
-    unknown = []
-    for vendor in vendors:
-        if vendor['display'] == '1':
-            if vendor['drop'] == '2':
-                good.append(vendor_names[vendor['shorthand']])
-            elif vendor['drop'] == '1':
-                bad.append(vendor_names[vendor['shorthand']])
-            else:
-                unknown.append(vendor_names[vendor['shorthand']])
-    
-    title = 'Vendor Engrams'
-    embed = discord.Embed(title=title)
-    if len(good) > 0:
-        embed.add_field(name='High', value="\n".join(good), inline=True)
-    if len(bad) > 0:
-        embed.add_field(name='Low', value="\n".join(bad), inline=True)
-    if len(unknown) > 0:
-        embed.add_field(name='Unknown', value="\n".join(unknown), inline=True)
-    embed.set_footer(text='https://vendorengrams.xyz/')
     await ctx.send(embed=embed)
 
 @bot.command(brief="Wheel of Fortune. But for raids.")
